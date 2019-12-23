4ed0e24124588c17b1ec8e0306f701a48ee67188
Gene Hunter <genehunter29009@gmail.com>
2019-12-23 08:41:35 -0500

    Create DT-GetusersbyCity.ps1

---
+++ DT-GetusersbyCity.ps1
@@ -0,0 +1,17 @@
+clear
+### replace ###SITEID### with your uniqueue site id. replace ###API-TOKEN### with your api key
+#needed in order to connect 
+$AllProtocols = [System.Net.SecurityProtocolType]'Ssl3,Tls,Tls11,Tls12'
+[System.Net.ServicePointManager]::SecurityProtocol = $AllProtocols
+## setup uri and header
+$uri = "https://###SITEID###.live.dynatrace.com/api/v1/userSessionQueryLanguage/table?query=SELECT%20country%2C%20city%2C%20COUNT(*)%20FROM%20usersession%20GROUP%20BY%20country%2C%20city&explain=false&Api-Token=###APITOKEN###"
+## execute api, save results into $results
+$results = Invoke-RestMethod -Method GET -Uri $uri 
+#$results | ConvertTo-Json
+#$results.values
+#$results | ConvertTo-Json
+#$results.values
+foreach($result in $results.values)
+{
+"$result[0],$result[1],$result[2]" | write-host
+}
